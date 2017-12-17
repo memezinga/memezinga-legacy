040a59c38e1e7a1224c6ba1a68db4880dc894893
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-12-17 08:55:55 +0000

    Add JSlinter task. #7

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var gulp = require('gulp'),
+    eslint = require('gulp-eslint'),
+    debug = require('gulp-debug');
+
+gulp.task('lint', function() {
+    var filesToLint = [
+        '**/*.{html,js}',
+        '!tests/protractor.conf.js',
+        '!dist/**/*',
+        '!docs/**/*',
+        '!node_modules/**/*',
+        '!tmp/**/*',
+        '!coverage/**/*'
+    ];
+
+    return gulp.src(filesToLint)
+            .pipe(debug({title: 'eslint (Scope):'}))
+            .pipe(eslint())
+            .pipe(eslint({fix:true}))
+            .pipe(eslint.format())
+            .pipe(gulp.dest('.'));
+});
