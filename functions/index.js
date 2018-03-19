f89304877efbc7d5152d2d8ab8a6bb1505b09c0f
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2018-03-19 17:30:20 +0000

    API Endpoint in firebase. Related #46

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'),
+    request = require('request');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.api = functions.https.onRequest((req, res) => {
+    request("https://api.imgflip.com/get_memes", { json: true }, (error, response, body) => {
+
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   
+        if (error) { 
+            console.log(error)
+            res.json(JSON.stringify(error))
+        } else {
+            res.json(body)
+        }
+
+    });
+});
